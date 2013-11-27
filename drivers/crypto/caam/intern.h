2f9753b931e5faf7f7bb6b763d6d85d7d50ba75d
Dan Douglass <dan.douglass@freescale.com>
2013-11-27 03:40:44 -0600

    ENGR00289885 [iMX6Q] Add Secure Memory and SECVIO support.
    1. Pull in secure memory support from 3.0.35 kernel.
    2. Pull in SECVIO support from 3.0.35 kernel.
    3. Make changes to support device tree.
    4. Add device tree setting for SECVIO sources.
    
    [<vicki.milhoan@freescale.com>: Edited to apply to 3.14]
    Signed-off-by: Dan Douglass <b41520@freescale.com>
    (cherry picked from commit f3bfd42e2db3af8326734bebf750e94e74734f6e)
    Signed-off-by: Victoria Milhoan <vicki.milhoan@freescale.com>
    Signed-off-by: Dan Douglass <dan.douglass@freescale.com>
    (cherry picked from commit 2c8eff8e55c2351288ba636b631fcb21c91d55bc)
    Signed-off-by: Aleksander Morgado <aleksander@aleksander.es>

--- drivers/crypto/caam/intern.h
+++ drivers/crypto/caam/intern.h
@@ -68,6 +68,8 @@ struct caam_drv_private {
 #ifdef CONFIG_CAAM_QI
 	struct device *qidev;
 #endif
+	struct device *smdev;
+	struct device *secviodev;
 
 	/* Physical-presence section */
 	struct caam_ctrl __iomem *ctrl; /* controller region */
@@ -75,6 +77,9 @@ struct caam_drv_private {
 	struct caam_assurance __iomem *assure;
 	struct caam_queue_if __iomem *qi; /* QI control region */
 	struct caam_job_ring __iomem *jr[4];	/* JobR's register space */
+	struct snvs_full __iomem *snvs;	/* SNVS HP+LP register space */
+	dma_addr_t __iomem *sm_base;	/* Secure memory storage base */
+	u32 sm_size;
 
 	/*
 	 * Detected geometry block. Filled in from device tree if powerpc,
