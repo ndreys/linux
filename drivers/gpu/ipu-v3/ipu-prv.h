ce89b3577e76bb829d6a8b5d820c96bb93a6d378
Philipp Zabel <p.zabel@pengutronix.de>
2014-08-07 18:43:11 +0200

    gpu: ipu-v3: add ipu_ic_csi_mem_wr_en
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>

--- drivers/gpu/ipu-v3/ipu-prv.h
+++ drivers/gpu/ipu-v3/ipu-prv.h
@@ -233,6 +233,7 @@ void ipu_csi_exit(struct ipu_soc *ipu, int id);
 int ipu_ic_init(struct ipu_soc *ipu, struct device *dev,
 		unsigned long base, unsigned long tpmem_base);
 void ipu_ic_exit(struct ipu_soc *ipu);
+void ipu_ic_csi_mem_wr_en(struct ipu_ic_priv *priv, bool mem_wr_en);
 
 int ipu_vdi_init(struct ipu_soc *ipu, struct device *dev,
 		 unsigned long base, u32 module);
