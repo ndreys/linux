914b9421885c724ffcb881f24f676dee0f83a862
Andrew Lunn <andrew@lunn.ch>
2015-12-27 16:11:21 -0600

    phy: Add phydev_err() and phydev_dbg() macros
    In preparation for moving some of the phy_device structure members,
    add macros for printing errors and debug information.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -771,6 +771,16 @@ static inline int phy_read_status(struct phy_device *phydev)
 	return phydev->drv->read_status(phydev);
 }
 
+#define phydev_err(_phydev, format, args...)	\
+do {						\
+	dev_err(&_phydev->dev, format, ##args);	\
+} while (0)
+
+#define phydev_dbg(_phydev, format, args...)	\
+do {						\
+	dev_dbg(&_phydev->dev, format, ##args);	\
+} while (0)
+
 int genphy_config_init(struct phy_device *phydev);
 int genphy_setup_forced(struct phy_device *phydev);
 int genphy_restart_aneg(struct phy_device *phydev);
