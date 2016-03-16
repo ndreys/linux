6ef412550fbf4f01b35a37ebfb01ef9915d9af8b
Andrew Lunn <andrew@lunn.ch>
2016-03-16 12:05:06 -0500

    dsa: Traverse the tree to determine if it is compete
    Every time a switch is added to the switch tree, traverse the tree and
    see if all the DSA port links are complete. That is, the other end of
    a DSA link is known within the tree.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>

--- net/dsa/dsa2.c
+++ net/dsa/dsa2.c
@@ -73,6 +73,146 @@ static void dsa_dst_del_ds(struct dsa_switch_tree *dst,
 	kref_put(&dst->refcount, dsa_free_dst);
 }
 
+static bool dsa_port_is_dsa(struct device_node *port)
+{
+	const char *name;
+
+	name = of_get_property(port, "label", NULL);
+	if (!name)
+		return false;
+
+	if (!strcmp(name, "dsa"))
+		return true;
+
+	return false;
+}
+
+static bool dsa_ds_find_port(struct dsa_switch *ds,
+			     struct device_node *port)
+{
+	u32 index;
+
+	for (index = 0; index < DSA_MAX_PORTS; index++)
+		if (ds->ports[index].dn == port)
+			return true;
+	return false;
+}
+
+static bool dsa_dst_find_port(struct dsa_switch_tree *dst,
+			      struct device_node *port)
+{
+	struct dsa_switch *ds;
+	u32 index;
+
+	for (index = 0; index < DSA_MAX_SWITCHES; index++) {
+		ds = dst->ds[index];
+		if (!ds)
+			continue;
+
+		if (dsa_ds_find_port(ds, port))
+			return true;
+	}
+
+	return false;
+}
+
+static int dsa_port_complete(struct dsa_switch_tree *dst,
+			     struct device_node *port)
+{
+	struct device_node *link;
+	int index;
+	bool found;
+
+	for (index = 0;; index++) {
+		link = of_parse_phandle(port, "link", index);
+		if (!link)
+			break;
+
+		found = dsa_dst_find_port(dst, link);
+		of_node_put(link);
+
+		if (!found)
+			return 1;
+
+		pr_debug("DSA: port %s to %s complete\n",
+			 port->full_name, link->full_name);
+	}
+
+	pr_debug("DSA: port %s complete\n", port->full_name);
+
+	return 0;
+}
+
+/* A switch is complete if all the DSA ports phandles point to ports
+ * known in the tree. A return value of 1 means the tree is not
+ * complete. This is not an error condition. A value of 0 is
+ * success.
+ */
+static int dsa_ds_complete(struct dsa_switch_tree *dst, struct dsa_switch *ds)
+{
+	struct device_node *port;
+	int err;
+
+	for_each_available_child_of_node(ds->dev->of_node, port) {
+		if (!dsa_port_is_dsa(port))
+			continue;
+
+		err = dsa_port_complete(dst, port);
+		if (err != 0)
+			return err;
+	}
+
+	pr_debug("DSA: switch %d %d complete\n", dst->tree, ds->index);
+
+	return 0;
+}
+
+/* A tree is complete if all the DSA ports phandles point to ports
+ * known in the tree. A return value of 1 means the tree is not
+ * complete. This is not an error condition. A value of 0 is
+ * success.
+ */
+static int dsa_dst_complete(struct dsa_switch_tree *dst)
+{
+	struct dsa_switch *ds;
+	u32 index;
+	int err;
+
+	for (index = 0; index < DSA_MAX_SWITCHES; index++) {
+		ds = dst->ds[index];
+		if (!ds)
+			continue;
+
+		err = dsa_ds_complete(dst, ds);
+		if (err != 0)
+			return err;
+	}
+
+	pr_debug("DSA: tree %d complete\n", dst->tree);
+
+	return 0;
+}
+
+static int dsa_parse_children_dn(struct device_node *np, struct dsa_switch *ds)
+{
+	struct device_node *port;
+	int err;
+	u32 reg;
+
+	for_each_available_child_of_node(np, port) {
+		err = of_property_read_u32(port, "reg", &reg);
+		if (err)
+			return err;
+
+		if (reg >= DSA_MAX_PORTS)
+			return -EINVAL;
+
+		ds->ports[reg].dn = port;
+	}
+
+	return 0;
+}
+
 static int dsa_parse_member(struct device_node *np, u32 *tree, u32 *index)
 {
 	int err;
@@ -101,6 +241,10 @@ static int _dsa_register_switch(struct dsa_switch *ds, struct device_node *np)
 	if (err)
 		return err;
 
+	err = dsa_parse_children_dn(np, ds);
+	if (err)
+		return err;
+
 	dst = dsa_get_dst(tree);
 	if (!dst) {
 		dst = dsa_add_dst(tree);
@@ -109,17 +253,23 @@ static int _dsa_register_switch(struct dsa_switch *ds, struct device_node *np)
 	}
 
 	if (dst->ds[index]) {
-		dsa_put_dst(dst);
-		return -EBUSY;
+		err = -EBUSY;
+		goto out;
 	}
 
 	ds->dst = dst;
 	ds->index = index;
 	dsa_dst_add_ds(dst, ds, index);
 
+	err = dsa_dst_complete(dst);
+	if (err < 0)
+		goto out;
+	err = 0;
+
+out:
 	dsa_put_dst(dst);
 
-	return 0;
+	return err;
 }
 
 int dsa_register_switch(struct dsa_switch *ds, struct device_node *np)
