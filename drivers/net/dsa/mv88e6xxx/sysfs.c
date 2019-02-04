a4b96f83a96e3bf7e9660288c90a9f82158011a8
Vivien Didelot <vivien.didelot@gmail.com>
2019-02-04 16:15:27 -0500

    zii: net: dsa: mv88e6xxx: add bpdu_bypass sysfs attribute
    Expose a sysfs entry that allows for enabling/disabling BPDU
    bypass. The method for doing this is to set bit 0 of Global 2 register
    3 to a 1 when BPDU bypass is disabled and to a 0 when BPDU bypass is
    enabled, so that frames with the destination address 01:80:C2:00:00:00
    are either sent to the CPU port or not.
    
    Signed-off-by: Vivien Didelot <vivien.didelot@gmail.com>

--- drivers/net/dsa/mv88e6xxx/sysfs.c
+++ drivers/net/dsa/mv88e6xxx/sysfs.c
@@ -13,8 +13,68 @@
 #include <linux/sysfs.h>
 
 #include "chip.h"
+#include "global2.h"
+
+#if IS_ENABLED(CONFIG_NET_DSA_MV88E6XXX_ZII_RDU)
+static int mv88e6xxx_g2_mgmt_en_00(struct mv88e6xxx_chip *chip, bool enabled)
+{
+	u16 val;
+	int err;
+
+	err = mv88e6xxx_g2_read(chip, MV88E6XXX_G2_MGMT_EN_0X, &val);
+	if (err)
+		return err;
+
+	if (enabled)
+		val &= ~BIT(0);
+	else
+		val |= BIT(0);
+
+	return mv88e6xxx_g2_write(chip, MV88E6XXX_G2_MGMT_EN_0X, val);
+}
+
+static ssize_t bpdu_bypass_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	struct dsa_switch *ds = dev_get_drvdata(dev);
+	struct mv88e6xxx_chip *chip = ds->priv;
+	u16 val;
+	int err;
+
+	mutex_lock(&chip->reg_lock);
+	err = mv88e6xxx_g2_read(chip, MV88E6XXX_G2_MGMT_EN_0X, &val);
+	mutex_unlock(&chip->reg_lock);
+	if (err)
+		return err;
+
+	return snprintf(buf, PAGE_SIZE, "%u\n", !(val & BIT(0)));
+}
+
+static ssize_t bpdu_bypass_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+{
+	struct dsa_switch *ds = dev_get_drvdata(dev);
+	struct mv88e6xxx_chip *chip = ds->priv;
+	unsigned int enabled;
+	int err;
+
+	if (sscanf(buf, "%u", &enabled) != 1 || enabled > 1)
+		return -EINVAL;
+
+	mutex_lock(&chip->reg_lock);
+	err = mv88e6xxx_g2_mgmt_en_00(chip, enabled);
+	mutex_unlock(&chip->reg_lock);
+	if (err)
+		return err;
+
+	return count;
+}
+
+static DEVICE_ATTR_RW(bpdu_bypass);
+#endif
 
 static struct attribute *mv88e6xxx_sysfs_attrs[] = {
+#if IS_ENABLED(CONFIG_NET_DSA_MV88E6XXX_ZII_RDU)
+	&dev_attr_bpdu_bypass.attr,
+#endif
 	NULL
 };
 
