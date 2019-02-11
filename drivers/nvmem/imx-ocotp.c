66fec4d8b6aee6ec0c6d7709e52afbdd3da01a06
Lucas Stach <l.stach@pengutronix.de>
2019-02-11 14:34:17 +0100

    nvmem: imx-ocotp: add support for imx8mq
    The i.MX8MQ uses the same OCOTP block as the i.MX7D, but with
    fourfold increase in fuse banks.
    
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>

--- drivers/nvmem/imx-ocotp.c
+++ drivers/nvmem/imx-ocotp.c
@@ -433,6 +433,12 @@ static const struct ocotp_params imx7d_params = {
 	.set_timing = imx_ocotp_set_imx7_timing,
 };
 
+static const struct ocotp_params imx8mq_params = {
+	.nregs = 256,
+	.bank_address_words = 4,
+	.set_timing = imx_ocotp_set_imx7_timing,
+};
+
 static const struct of_device_id imx_ocotp_dt_ids[] = {
 	{ .compatible = "fsl,imx6q-ocotp",  .data = &imx6q_params },
 	{ .compatible = "fsl,imx6sl-ocotp", .data = &imx6sl_params },
@@ -440,6 +446,7 @@ static const struct of_device_id imx_ocotp_dt_ids[] = {
 	{ .compatible = "fsl,imx6ul-ocotp", .data = &imx6ul_params },
 	{ .compatible = "fsl,imx7d-ocotp",  .data = &imx7d_params },
 	{ .compatible = "fsl,imx6sll-ocotp", .data = &imx6sll_params },
+	{ .compatible = "fsl,imx8mq-ocotp", .data = &imx8mq_params },
 	{ },
 };
 MODULE_DEVICE_TABLE(of, imx_ocotp_dt_ids);
